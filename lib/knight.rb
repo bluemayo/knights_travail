cbe62d244ff84359f46896cbf2ad633a034fc965
Dylan <152871121+bluemayo@users.noreply.github.com>
2024-04-26 19:44:18 +0800

    Class Knight can be initialized with positions of possible moves

---
+++ lib/knight.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+# defines the position of a knight and where it can move
+class Knight
+  attr_accessor :at, :possible
+
+  def initialize(at)
+    @at = at
+    @possible = calc_possible(at[0], at[1])
+    p possible
+  end
+
+  private
+
+  def calc_possible(x, y) # rubocop:disable Naming/MethodParameterName
+    to = []
+    x_array = calc_x_array(x)
+    x_array.each do |x_number|
+      diff = x < x_number ? 3 - x_number - x : 3 - x - x_number
+      y_array = calc_y_array(y, diff)
+      y_array.each do |y_number|
+        to << [x_number, y_number]
+      end
+    end
+    to
+  end
+
+  def calc_x_array(x) # rubocop:disable Naming/MethodParameterName
+    array = (x - 2..x + 2).to_a
+    array.delete_at(2)
+    array.reject { |position| position.negative? || position > 7 }
+  end
+
+  def calc_y_array(y, diff) # rubocop:disable Naming/MethodParameterName
+    array = Array.new(2, y)
+    array[0] -= diff
+    array[1] += diff
+    array.reject { |position| position.negative? || position > 7 }
+  end
+end
+
+Knight.new([0, 0])
